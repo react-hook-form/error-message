b98baf235fa572afbc97da8341874910eafb1859
Kotaro Sugawara <kotarella1110@gmail.com>
2020-06-16 13:01:12 +0900

    change umd name from HookFormErrorMessage to ReactHookFormErrorMessage

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ export default [
   {
     input: 'src/index.tsx',
     output: {
-      name: 'HookFormErrorMessage',
+      name: 'ReactHookFormErrorMessage',
       file: pkg.unpkg,
       format: 'umd',
       sourcemap: true,
